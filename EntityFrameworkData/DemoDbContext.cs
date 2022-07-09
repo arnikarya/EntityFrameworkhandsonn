ea8c058960dfbe9b08f2046ae3d85f3372820018
Acer <myemail>
2022-07-09 23:39:35 +0700

    Add project files.

---
+++ EntityFrameworkData/DemoDbContext.cs
@@ -0,0 +1,22 @@
+﻿using EntityFrameworkData.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameworkData
+{
+    public class DemoDbContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<EmployeeEducation> EmployeeEducations { get; set; }
+       public DemoDbContext()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=LAPTOP-QQUJ36KT\MSSQLSERVER01;Database=EntityFrameworkDb;Trusted_Connection=True;");
+
+
+        }
+    }
+}
